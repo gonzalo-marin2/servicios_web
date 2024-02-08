d9dfa9047f98cdaf51fd55084481ef5e02a0021e
tarde <tarde@A13E34>
2024-02-08 20:54:57 +0100

    adición anotación CORS @CROSSORIGIN("*")

--- 04_servicio_learning/src/main/java/controller/FormacionController.java
+++ 04_servicio_learning/src/main/java/controller/FormacionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import model.Formacion;
 import service.interfaces.FormacionService;
 
+@CrossOrigin("*")
 @RestController
 public class FormacionController {
 	@Autowired
