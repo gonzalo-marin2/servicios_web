cd72bbc764c00de2b81f10cacb6fc5c582cd2e55
tarde <tarde@A13E34>
2024-02-12 20:51:43 +0100

    proyecto 11

---
+++ 11_microservicio_learning/src/main/java/init/model/Formacion.java
@@ -0,0 +1,23 @@
+package init.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Formacion {
+	@JsonProperty(value = "denominacion")//trabaja en ambos sentidos
+	/*//usamos la anotación para darle a nombre el valor denominacion que es el que está
+	//en cursos. Sólo serialización
+	@JsonAlias(value = "denominacion")*/
+	private String nombre;
+	@JsonProperty(value = "duracion")
+	//@JsonAlias(value = "duracion")
+	private int horas;
+	private double precio;
+}
