3ac7f49dab8e3baae43b27d39c6ddf3b7395bc9f
tarde <tarde@A13E34>
2024-02-12 20:29:09 +0100

    proyecto 10 finalizado

---
+++ 10_microservicio_crud_cursos/src/main/java/init/dao/CursosDao.java
@@ -0,0 +1,32 @@
+package init.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import init.model.Curso;
+
+public interface CursosDao extends JpaRepository<Curso,Integer> {
+	
+	List<Curso> findByPrecioBetween(double p1, double p2);
+	
+	Curso findByDenominacion(String denominacion);//lo ponemos por si luego nos hace falta
+	
+	@Transactional
+	@Modifying
+	void deleteByDenominacion(String denominacion);
+	//los delete y los update son void, se modifica en la lógica de negocio
+	
+	@Transactional
+	@Modifying
+	@Query("update Curso c set c.precio=c.precio*(100+?1)/100 where c.denominacion=?2")
+	void updatePrecio(int porcentaje, String denominacion);
+	
+	//heredados
+	//List<Curso> findAll();
+	//Curso findByIdCurso(int idCurso);
+	//List<Curso> save(Curso curso); este método tb actualiza un registro
+}
---
+++ 10_microservicio_crud_cursos/src/main/java/init/model/Curso.java
@@ -0,0 +1,26 @@
+package init.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="cursos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Curso {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idCurso;
+	private String denominacion;
+	private int duracion;
+	private double precio;
+	
+}
