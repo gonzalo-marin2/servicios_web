8f62f77229d67a8b7e0e2581ba7ff0d74cf10e3c
tarde <tarde@A13E34>
2024-02-12 20:50:31 +0100

    Proyecto terminado pero da error, revisar

---
+++ 09_microservicio_rastreator/src/main/java/init/controller/VehiculosController.java
@@ -0,0 +1,32 @@
+package init.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import init.model.Vehiculo;
+import init.service.interfaces.VehiculosService;
+
+@CrossOrigin("*")//permitimos llamadas desde cualquier origen
+@RestController
+public class VehiculosController {
+
+	@Autowired
+	VehiculosService service;
+	
+	@GetMapping(value="buscarPorPrecioMax/{precioMax}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Vehiculo> buscarPorPrecioMax(@PathVariable("precioMax") double precioMax){
+		return service.buscarPorPrecioMax(precioMax);
+	}
+	
+	@GetMapping(value="buscarPorRangoKilometros/{minimo}/{maximo}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Vehiculo> buscarPorRangoKilometros(@PathVariable("minimo") int minimo, @PathVariable("maximo") int maximo){
+		return service.buscarPorRangoKilometros(minimo, maximo);
+	}
+	
+}
