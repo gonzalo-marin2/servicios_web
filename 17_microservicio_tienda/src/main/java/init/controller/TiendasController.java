3cb66c5853c5a3562279c6a4f952e8f9d5dc27bc
tarde <tarde@A13E34>
2024-02-14 19:37:16 +0100

    commit proyecto 17 finzalizado con errores

---
+++ 17_microservicio_tienda/src/main/java/init/controller/TiendasController.java
@@ -0,0 +1,33 @@
+package init.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import init.model.Libro;
+import init.service.interfaces.TiendasService;
+
+@CrossOrigin("*")
+@RestController
+public class TiendasController {
+	
+	@Autowired
+	TiendasService tiendasService;
+	
+	@GetMapping(value = "tematicas",produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<String>> tematicas(){
+		return new ResponseEntity<>(tiendasService.tematicas(),HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "libros/{tematica}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Libro> librosPorTematica(@PathVariable("tematica") String tematica){
+		return tiendasService.librosPorTematica(tematica);
+	}
+}
